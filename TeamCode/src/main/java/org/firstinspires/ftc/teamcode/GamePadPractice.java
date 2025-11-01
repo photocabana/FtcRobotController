9015886d32238d818efcec5426e5306561ba5953
Rindy Fisk <117949294+photocabana@users.noreply.github.com>
2025-11-01 17:33:33 -0500

    Update GamePadPractice.java
    Android Studio Game pad practice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+@Disabled
 @TeleOp
 public class GamePadPractice extends OpMode {
 
@@ -14,8 +15,27 @@ public class GamePadPractice extends OpMode {
     @Override
     public void loop() {
         // runs 50x a second
-        telemetry.addData("x", gamepad1.left_stick_x);
-        telemetry.addData("y", gamepad1.left_stick_y);
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        double diffXJoysticks = gamepad1.left_stick_x - gamepad1.right_stick_x;
+        double sumTriggers = gamepad1.left_trigger + gamepad1.right_trigger;
+
+        telemetry.addData("left x", gamepad1.left_stick_x);
+        // telemetry.addData("left y", gamepad1.left_stick_y);
+        telemetry.addData("left y", speedForward);
+        telemetry.addData("right x",gamepad1.right_stick_x);
+        telemetry.addData("right y", gamepad1.right_stick_y);
+        telemetry.addData("difference x", diffXJoysticks);
+
         telemetry.addData("a button", gamepad1.a);
+        telemetry.addData("b button", gamepad1.b);
+
+        telemetry.addData("sum triggers", sumTriggers);
     }
+
+    /*
+    1. add telemetry for the right joystick
+    2. add telemetry for the B button
+    3. add telemetry data to report the DIFFERENCE between X left joystick and X right joystick.
+    4. add telemetry data to report the sum of both rear triggers.
+     */
 }
